f5ac9eb43fdc978af2bb61df30abb5add26e2ef5
Matthew W Burnard <oldenglandimmigrant@gmail.com>
2025-01-26 14:31:35 -0800

    CH2: Gameloop.

--- main.py
+++ main.py
@@ -2,9 +2,24 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    init_result = pygame.init()
+    print(f"pygame init results; num_pass:{init_result[0]}, num_fail:{init_result[1]}")
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill((0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
