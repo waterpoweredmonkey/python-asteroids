1cdefb399aa903858d4ef56b4335ee038e37ad5a
Matthew W Burnard <oldenglandimmigrant@gmail.com>
2025-01-26 20:59:10 -0800

    CH2: Moving Around.

--- main.py
+++ main.py
@@ -18,9 +18,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        player.update(dt)
+        
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
