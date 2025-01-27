e21df8c8168b339d314f57c088ad49ca353e0e8c
Matthew W Burnard <oldenglandimmigrant@gmail.com>
2025-01-26 19:44:44 -0800

    CH2: Draw Player

--- main.py
+++ main.py
@@ -3,23 +3,25 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    init_result = pygame.init()
-    print(f"pygame init results; num_pass:{init_result[0]}, num_fail:{init_result[1]}")
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill((0,0,0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
